392865601f732811aaf3c2530e8addba0fa60477
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-12-05 14:10:38 +0100

    Add init test

---
+++ src/test/java/com/example/gruppajava/GruppAJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppAJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
