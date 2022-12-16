1925c9c6f8c1b07138a85a82af76d910c4d5e0db
FredricNordin <90624100+FredricNordin@users.noreply.github.com>
2022-12-16 17:15:45 +0100

    Update ParkingEventController.java

--- src/main/java/com/example/gruppajava/controller/ParkingEventController.java
+++ src/main/java/com/example/gruppajava/controller/ParkingEventController.java
@@ -1,6 +1,7 @@
 package com.example.gruppajava.controller;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,47 +12,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.gruppajava.entity.ParkingEvent;
+import com.example.gruppajava.repository.CarRepository;
 import com.example.gruppajava.repository.ParkingEventRepository;
+import com.example.gruppajava.repository.UserRepository;
+
 
 @RestController
-@RequestMapping("/api/parkingevents")
+@RequestMapping("/api/")
 public class ParkingEventController {
     @Autowired ParkingEventRepository parkingEventRepository;
+    @Autowired CarRepository carRepository;
+    @Autowired UserRepository userRepository;
 
     // Get all parking events.
-    @GetMapping("/")
+    @GetMapping("parkingevents")
     public Iterable<ParkingEvent> getAllParkingEvents() {
         return parkingEventRepository.findAll();
     }
-    
-    // Add a new parking event.
-    @PostMapping("/")
-    public ParkingEvent addParkingEvent(@RequestBody ParkingEvent parkingEvent) {
+
+    // Create a new parking event.
+    @PostMapping("parkingevents")
+    public ParkingEvent addParkingEvent(@RequestBody Map<String, String> body) {
+        Long carId = Long.parseLong(body.get("carId"));
+        Long userId = Long.parseLong(body.get("userId"));
+        int parkingslotId = Integer.parseInt(body.get("parkingslotId"));
+
+        ParkingEvent parkingEvent = new ParkingEvent();
+        parkingEvent.setCar(carRepository.findById(carId).get());
+        parkingEvent.setUser(userRepository.findById(userId).get());
+        parkingEvent.setSlotId(parkingslotId);
         parkingEvent.setStartTime(LocalDateTime.now());
         parkingEvent.setEndTime(LocalDateTime.now().plusMinutes(10));
         parkingEvent.setActive(true);
         parkingEvent.setPaid(false);
+        
         return parkingEventRepository.save(parkingEvent);
     }
 
     // Get parking event by id.
-    @GetMapping("/{id}")
+    @GetMapping("parkingevents/{id}")
     public ParkingEvent getParkingEventById(@PathVariable Long id) {
         return parkingEventRepository.findById(id).get();
     }
 
     // Get parking event by user id.
-    @GetMapping("/user/{id}")
+    @GetMapping("parkingevents/user/{id}")
     public Iterable<ParkingEvent> getParkingEventByUserId(@PathVariable Long id) {
         return parkingEventRepository.findByUserId(id);
     }
 
     // Get parking event by car id.
-    @GetMapping("/car/{id}")
-    public Iterable<ParkingEvent> getParkingEventByCarId(@PathVariable int id) /*<- Long id when car entity exists.*/ {
+    @GetMapping("parkingevents/car/{id}")
+    public Iterable<ParkingEvent> getParkingEventByCarId(@PathVariable Long id) {
         return parkingEventRepository.findByCarId(id);
     }
 
-    // Need more patch routes for updating parking events later... us boys have to talk about this :-)
+    // Need more patch routes for updating parking events later...
 
 }
