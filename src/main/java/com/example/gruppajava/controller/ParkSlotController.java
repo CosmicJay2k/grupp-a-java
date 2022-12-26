45aaa9cc8e7f8d69be6077371928ebdeba87385a
Ammar Amin <ammar.amin1975@gmail.com>
2022-12-26 12:26:20 +0100

    add get park slot by id

--- src/main/java/com/example/gruppajava/controller/ParkSlotController.java
+++ src/main/java/com/example/gruppajava/controller/ParkSlotController.java
@@ -3,6 +3,7 @@ package com.example.gruppajava.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.gruppajava.entity.ParkSlot;
@@ -24,5 +25,10 @@ public class ParkSlotController {
   return parkslotRepo.findAll();
   }
 
-  
+  // return one park slot depending on (id) parameter
+  @GetMapping("/api/parkslot/{id}")
+  public ParkSlot getParkSlot(@PathVariable Long id) {
+    return parkslotRepo.findById(id).get();
+  }
+
 }
