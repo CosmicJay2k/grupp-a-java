b99afb56c313f2a8f1081b68473cf3a18573570f
Ammar Amin <ammar.amin1975@gmail.com>
2022-12-27 01:23:12 +0100

    create test file and add first test-get ok status

---
+++ src/test/java/com/example/gruppajava/controller/ParkSlotControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.gruppajava.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import com.example.gruppajava.repository.ParkSlotRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ParkSlotController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ParkSlotControllerTest {
+  
+  @MockBean
+  ParkSlotRepository parkslotRepository;
+
+//  @MockBean
+//  ParkPriceZoneRepository parkpricezoneRepository;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void getParkSlotShouldReturn200OK() throws Exception {
+      mvc.perform(get("/api/parkslot"))
+        .andExpect(status().isOk());
+  }
+
+
+  public static String asJsonString(final Object obj) {
+    try {
+        return new ObjectMapper().writeValueAsString(obj);
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
+  }
+  
+}
+
