e3c137e1c616fcd5778167a47aa45ead1417258d
Marcus Larsson <marcus.b.larsson@outlook.com>
2022-12-28 12:31:34 +0100

    added secondary btns

--- frontend/src/Components/btns/SecondaryBtn.js
+++ frontend/src/Components/btns/SecondaryBtn.js
@@ -1,6 +1,6 @@
-
+import './SecondaryBtn.css'
 export default function SecondaryBtn({text, clickFn}){
   return (
-    <button onClick={clickFn}> {text} </button>
+    <button className="secondary-btn" onClick={clickFn}> {text} </button>
   )
 }
