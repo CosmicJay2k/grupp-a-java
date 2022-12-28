4f0bf128842608f17443cb1b67eb629307eb2c4b
Marcus Larsson <marcus.b.larsson@outlook.com>
2022-12-28 12:52:11 +0100

    changed app.js file to start with a MyPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
+import MyPage from "Components/MyPage/MyPage";
 import { useState } from "react";
-import LoginForm from "Components/LoginForm/LoginForm";
-import RegisterForm from "Components/RegisterForm/RegisterForm";
-import PrimaryCardLayout from "Layouts/PrimaryCardLayout/PrimaryCardLayout";
 import 'style/App.css'
-import PrimaryHeader from "Components/Headers/PrimaryHeader";
-import SecondaryHeader from "Components/Headers/SecondaryHeader"
 
 function App() {
-  const [switchForm, setSwitchForm] = useState(true)
-
-  function handleChangeForm(){
-    setSwitchForm(prev => !prev)
-  }
-
   return (
     <div className="App">
-      { switchForm ? <PrimaryHeader text='login'/> : <SecondaryHeader text='register' /> }
-      <PrimaryCardLayout>
-        {
-          switchForm ? <LoginForm changeForm={handleChangeForm}  /> : <RegisterForm changeForm={handleChangeForm}/>  
-        }
-      </PrimaryCardLayout>
+      <MyPage userName="Parker Portslot" />
     </div>
   );
 }
