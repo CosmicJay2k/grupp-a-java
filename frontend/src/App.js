1b7bb96b33f249c5b3390bf7e92430071f315aa2
Marcus Larsson <marcus.b.larsson@outlook.com>
2022-12-28 17:21:16 +0100

    added logo header to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import LogoHeader from "Components/Headers/LogoHeader";
 import MyPage from "Components/MyPage/MyPage";
-import { useState } from "react";
 import 'style/App.css'
 
 function App() {
   return (
     <div className="App">
+      <LogoHeader/>
       <MyPage userName="Parker Portslot" />
     </div>
   );
